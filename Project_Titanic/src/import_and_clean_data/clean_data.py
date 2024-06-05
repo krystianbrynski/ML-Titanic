359492564a8ede2ca5937f939ae180e2d26221c7
krystianbrynski <krystian.brynski@student.put.poznan.pl>
2024-06-05 20:17:12 +0200

    Added functionality - clean both datasets

--- Project_Titanic/src/import_and_clean_data/clean_data.py
+++ Project_Titanic/src/import_and_clean_data/clean_data.py
@@ -1,2 +1,32 @@
 import pandas as pd
 
+def clean_data(data):
+    data = data.drop("PassengerId", axis=1)
+    data = data.drop("Cabin", axis=1)
+    data = data.drop("Name", axis=1)
+    data = data.drop("Ticket", axis=1)
+
+    data["Age"] = data["Age"].fillna(data["Age"].mean())
+    data["Age"] = data["Age"].astype("int")
+
+    data["Sex_binary"] = 0
+    data["Sex_binary"] = data["Sex_binary"].where(data["Sex"] == "male", 1)
+    data = data.drop("Sex", axis=1)
+
+    x = pd.get_dummies(data["Embarked"]).astype("int")
+    data=data.assign(
+    C = x["C"],
+    Q = x["Q"],
+    S = x["S"],
+    )
+
+    data = data.drop("Embarked", axis=1)
+
+    #Y = data["Survived"]
+    #X = data.iloc[:,1:]
+
+    return (data)
+
+def replace_NAN_ticket(data):
+    data["Fare"] = data["Fare"].fillna(data["Fare"].mean())
+    return data
--- Project_Titanic/src/main.py
+++ Project_Titanic/src/main.py
@@ -1,9 +1,21 @@
-from import_and_clean_data import import_data
+from import_and_clean_data import import_data , clean_data
 
 Start = 0
 
 while Start !=1 :
        Start = int(input("This is my first Project in python if you want to load data press 1: "))
 
-import_data.read_train_data()
-import_data.read_test_data()
+train_data = import_data.read_train_data()
+test_data = import_data.read_test_data()
+
+clean_data_train = clean_data.clean_data(train_data)
+
+almost_clean_data_test = clean_data.clean_data(test_data)
+clean_data_test = clean_data.replace_NAN_ticket(almost_clean_data_test)
+
+X_train = clean_data_train["Survived"]
+Y_train = clean_data_train.iloc[:,1:]
+
+X_test = clean_data_test["Survived"]
+Y_test = clean_data_test.iloc[:,1:]
+
